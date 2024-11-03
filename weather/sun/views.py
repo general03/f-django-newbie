15c8b83e28f40cedee4d302832639afeb099ed41
david <david@itarverne.fr>
2024-11-03 19:09:17 +0100

    feat: update model with FK

--- weather/sun/views.py
+++ weather/sun/views.py
@@ -1,3 +1,18 @@
+import json
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from sun.models import Sun
+
+# def suns(request):
+#     data = []
+#     for row in Sun.objects.all():
+#         data.append(row.id)
+
+#     return JsonResponse(data={"content": data})
+
+
+def suns(request):
+    data = Sun.objects.all()
+    return render(request, "sun.html", {"data": data})
+
