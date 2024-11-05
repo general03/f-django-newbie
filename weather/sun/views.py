d8546557283867c2adf601330577331451ba424c
david <david@itarverne.fr>
2024-11-05 20:26:11 +0100

    feat: add url with ReST concept

--- weather/sun/views.py
+++ weather/sun/views.py
@@ -12,7 +12,20 @@ from sun.models import Sun
 #     return JsonResponse(data={"content": data})
 
 
-def suns(request):
-    data = Sun.objects.all()
-    return render(request, "sun.html", {"data": data})
+# def suns(request):
+#     data = Sun.objects.all()
+#     return render(request, "sun.html", {"data": data})
 
+
+# def suns(request):
+#     """Route with GET parameter"""
+#     if request.method == "GET":
+#         country_name = request.GET.get("country", "Paris")
+#         data = Sun.objects.filter(country=country_name)
+#     return render(request, "sun.html", {"data": data})
+
+def suns(request, country):
+    """Route with GET parameter structured like API ReST"""
+    if request.method == "GET":
+        data = Sun.objects.filter(country=country)
+    return render(request, "sun.html", {"data": data})
--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from sun.views import suns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("sun/", suns),
+    path("suns/<str:country>", suns),
+    # re_path(r"suns/(?P<country>[A-Z]{1}[a-z]+)$", suns),
 ]
