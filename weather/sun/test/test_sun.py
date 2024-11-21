6c23537bdd1e6af2576bac640a13a86c24c573b4
david <david@itarverne.fr>
2024-11-21 19:47:26 +0100

    feat: add first test function

--- weather/sun/test/test_sun.py
+++ weather/sun/test/test_sun.py
@@ -1,2 +1,9 @@
-def test_first():
-    assert True
+import pytest 
+
+@pytest.mark.parametrize("data,expected", [((3, 5), 8), ((2, 4), 6), ((6, 9), 15)])
+def test_add(data, expected):
+    assert add(*data) == expected
+
+def add(a, b):
+    """A ne pas mettre ici, c'est juste pour l'exemple..."""
+    return a + b
