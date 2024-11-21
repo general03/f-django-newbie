772e8106b5d435cec910e2c2b81ffc734d98086b
david <david@itarverne.fr>
2024-11-21 20:24:44 +0100

    feat: add test settings function

--- weather/sun/test/test_sun.py
+++ weather/sun/test/test_sun.py
@@ -1,3 +1,6 @@
 def test_health_check(client):
     """ > pipenv run pytest --ds=weather.settings """
     assert b'{"status": "ok"}' == client.get('/health/').content
+
+def test_app_settings(settings):
+    assert "sun" in settings.INSTALLED_APPS
